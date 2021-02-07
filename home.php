9ecd30dc47c130da65645671793fc3efea5cf1c8
johndaumentado <johndaumentado@gmail.com>
2021-02-07 20:50:46 +0800

    Second commit

---
+++ home.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link rel="stylesheet" href="style/homeStyle.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"</script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+</head>
+<body>
+<?php include('navbar.php'); ?>
+<div class="container-fluid">
+  <img src="img/placeholderIMG.png" alt="Home Image" style="width:100%;">
+  <div class="centered"><h1>Placeholder Text</h1> above Placeholder Image</div>
+</div><br/>
+<h1 align="center">Featured Developers</h1>
+
+
+<div class="container">
+  <div class="row">
+    <div class="col-sm">
+		<div class="img-with-text">
+			<img src="img/placeholderDEV.jpg" alt="sometext" width="150px" height="300px" />
+			<p>Developer 1</p>
+		</div>
+    </div>
+    <div class="col-sm">
+		<div class="img-with-text">
+			<img src="img/placeholderDEV.jpg" alt="sometext" width="150px" height="300px" />
+			<p>Developer 2</p>
+		</div>
+    </div>
+    <div class="col-sm">
+		<div class="img-with-text">
+			<img src="img/placeholderDEV.jpg" alt="sometext" width="150px" height="300px" />
+			<p>Developer 3</p>
+		</div>
+    </div>
+	<div class="col-sm">
+		<div class="img-with-text">
+			<img src="img/placeholderDEV.jpg" alt="sometext" width="150px" height="300px" />
+			<p>Developer 4</p>
+		</div>
+    </div>
+	<div class="col-sm">
+		<div class="img-with-text">
+			<img src="img/placeholderDEV.jpg" alt="sometext" width="150px" height="300px" />
+			<p>Developer 5</p>
+		</div>
+    </div>
+  </div>
+</div>
+
+</body>
+</html>
---
+++ navbar.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Bootstrap Example</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="style/navbarStyle.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+  <a class="navbar-brand" href="#">DevsLounge</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+	<ul class="navbar-nav ml-auto">
+	<li class="nav-item"><a class="nav-link" href="#">Log-In</a></li>
+	<li class="nav-item"><a class="nav-link" href="#">Link</a></li>
+	<li class="nav-item"><a class="nav-link" href="#">Get Started!</a></li>
+	</ul>
+  </div>
+</nav>
+</body>
+</html>
+  
